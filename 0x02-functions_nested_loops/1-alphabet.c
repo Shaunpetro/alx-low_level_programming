7248d68640d2183ada2dce0d3a11724e22415e4f
shaunpetro <shaun.p.mckoy@gmail.com>
2023-06-20 11:58:08 -0700

    task 2.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 
-	/**
-	 * print_aphabet - Make the alphabet
-	 * print_alphabet - print all alphabet in lowercase
-	 *
-	 * This function prints all the lowercase letters from 'a' to 'z',
-	 * followed by a new line character
-	 * Return: void
-	 */
-
+/**
+ * print_alphabet - print all alphabet in lowercase
+ *
+ * This function prints all the lowercase letters from 'a' to 'z',
+ * followed by a newline character.
+ *
+ * Return: void
+ */
 
 	void print_alphabet(void)
 	{
